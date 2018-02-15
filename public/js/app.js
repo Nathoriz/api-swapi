4eebea83b94640f81d1e3c9bcbe4ed16dd8d82e0
Nathoriz <nathortiz_41@hotmail.com>
2018-02-14 23:13:08 -0500

    Añadiendo modal

--- public/js/app.js
+++ public/js/app.js
@@ -29,10 +29,17 @@ function addNews() {
   result.forEach(function (re) {
     console.log(re);
     const name = re.name;
+    const height = re.height;
+    const mass = re.mass;
+    const eyesColor = re.eye_color;
+    const hairColor = re.hair_color;
+    const gender = re.gender;
+    const homeworld = re.homeworl;
+
 
     let div = document.createElement('div');
     div.className = 'col-sm-4';
-    div.setAttribute('id', '1'); 
+    div.setAttribute('data-name', name); 
     div.style.backgroundImage = "url('../assets/images/characters/" + name + ".jpg')";
     div.style.backgroundPosition = 'center center';
     div.style.backgroundRepeat = 'no repeat';
@@ -40,9 +47,40 @@ function addNews() {
     div.style.height = '25vh';
 
     responseContainer.appendChild(div);
-    console.log(data);
-    console.log(result);
-    console.log(name);
+    // console.log(data);
+    // console.log(result);
+    // console.log(name);
+
+    console.log(homeworld);
+
+    div.addEventListener('click', function() {
+      if(this.dataset.name === name) {
+        this.setAttribute('data-toggle', 'modal');
+        this.setAttribute('data-target', '#exampleModal');
+
+        let title = document.getElementById('title');
+        title.innerText = name;
+
+        let elementHeight = document.getElementById('height');
+        elementHeight.innerText = height;
+
+        let elementMass = document.getElementById('mass');
+        elementMass.innerText = mass;
+
+        let elementEyesColor = document.getElementById('eyesColor');
+        elementEyesColor.innerText = eyesColor;
+
+        let elementHairColor = document.getElementById('hairColor');
+        elementHairColor.innerText = hairColor;
+
+        let elementGender = document.getElementById('gender');
+        elementGender.innerText = gender;
+
+        let elementHomeworld = document.getElementById('homeworld');
+        elementHomeworld.innerText = homeworld;
+
+      }
+    })
   })
 
 }
